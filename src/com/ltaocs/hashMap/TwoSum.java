437300ff472150fdee9d32d9b0352f49554a27f2
Lin Tao <lintaoo@hotmail.com>
2018-12-01 13:55:16 -0800

    Leetcode 3. Longest Substring Without Repeating Characters

---
+++ src/com/ltaocs/hashMap/TwoSum.java
@@ -0,0 +1,29 @@
+package com.ltaocs.hashMap;
+
+import java.util.HashMap;
+
+/**
+ * Lin Tao
+ * a.ltaocs.com
+ * <p>
+ * Leetcode 1. Two Sum
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+            if (map.containsKey(num)) {
+                return new int[]{map.get(num), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
---
+++ src/com/ltaocs/set/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,43 @@
+package com.ltaocs.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Lin Tao
+ * a.ltaocs.com
+ * <p>
+ * Leetcode 3. Longest Substring Without Repeating Characters
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Example 1:
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * Example 3:
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        Set<Character> set = new HashSet<>();
+        while (i < len && j < len) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
